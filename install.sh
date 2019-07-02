80ce3994831937e8e8da4137decb9a26f64bf0ee
Phil Howard <phil@gadgetoid.com>
2019-07-02 10:47:01 +0100

    Improve install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 
-LIBRARY_VERSION=`cat library/setup.py | grep version | awk -F"'" '{print $2}'`
-LIBRARY_NAME=`cat library/setup.py | grep name | awk -F"'" '{print $2}'`
+LIBRARY_VERSION=`grep version library/setup.cfg | awk -F" = " '{print $2}'`
+LIBRARY_NAME=`grep name library/setup.cfg | awk -F" = " '{print $2}'`
+PY2_DEPS=`grep py2deps library/setup.cfg | awk -F" = " '{print $2}'`
+PY3_DEPS=`grep py3deps library/setup.cfg | awk -F" = " '{print $2}'`
+
+function apt_pkg_install {
+	PACKAGES=()
+	for PACKAGE in "$@"; do
+		printf "Checking for $PACKAGE\n"
+		dpkg -L $PACKAGE > /dev/null 2>&1
+		if [ "$?" == "1" ]; then
+			PACKAGES+=("$PACKAGE")
+		fi
+	done
+	PACKAGES="${PACKAGES[@]}"
+	if ! [ "$PACKAGES" == "" ]; then
+		echo "Installing missing packages: $PACKAGES"
+		sudo apt update
+		sudo apt install -y $PACKAGES
+	fi
+}
 
 printf "$LIBRARY_NAME $LIBRARY_VERSION Python Library: Installer\n\n"
 
@@ -13,10 +32,12 @@ fi
 cd library
 
 printf "Installing for Python 2..\n"
+apt_pkg_install $PY2_DEPS
 python setup.py install
 
 if [ -f "/usr/bin/python3" ]; then
 	printf "Installing for Python 3..\n"
+	apt_pkg_install $PY3_DEPS
 	python3 setup.py install
 fi
 
