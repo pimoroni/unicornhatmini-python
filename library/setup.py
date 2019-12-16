032e52179f50f98e48073f2eb0847d734035837d
Phil Howard <phil@pimoroni.com>
2019-12-16 12:16:47 +0000

    Shift setuptools requirements to pyproject.toml

--- library/setup.py
+++ library/setup.py
@@ -30,7 +30,4 @@ minimum_version = parse_version('30.4.0')
 if parse_version(__version__) < minimum_version:
     raise RuntimeError("Package setuptools must be at least version {}".format(minimum_version))
 
-setup(
-    packages=['{{LIBNAME}}'],
-    install_requires=['setuptools>={}'.format(minimum_version)]
-)
+setup()
